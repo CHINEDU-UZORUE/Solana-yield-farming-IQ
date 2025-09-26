97c842a15d43802dac625442bbec0f2a2f152774
Chinedu Uzorue <uzoruechinedu@gmail.com>
2025-09-26 02:45:44 +0100

    mod

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ ALLOWED_ORIGINS = os.getenv(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
